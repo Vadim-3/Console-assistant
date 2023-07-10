de5fe801556b667d9f509f9b3f274ed2aad5a383
Vadim-3 <128088822+Vadim-3@users.noreply.github.com>
2023-07-10 11:40:39 +0300

    1-commit

---
+++ res.py
@@ -0,0 +1,31 @@
+from address_book import AddressBook as ab
+from pathlib import Path
+
+while True:
+    ff = input('Введіть команду: ')
+    if ff == '1':
+        if Path(ab.filename).exists():
+            ab.unpackaging()
+            
+        while True:
+            command = input('введіть команду ')
+
+            if command == 'add':
+                name = input("введіть ім'я ")
+                address = input('Введіть адресу ')
+                email = input("введіть email ")
+                number = input("введіть number ")
+                birthday = input('Введіть введіть дату народження ')
+                ab.add_contact(name, address, email, number, birthday)
+                
+            if command == 'exit':
+                ab.packaging()
+                
+    if ff == '1':
+        pass
+    
+    if ff == '1':
+        pass
+    
+    if ff == '1':
+        pass
