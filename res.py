394ff91531cae99041493ba797e8fc5f25fb4b4b
Vadim-3 <128088822+Vadim-3@users.noreply.github.com>
2023-07-10 14:41:47 +0300

    4

--- res.py
+++ res.py
@@ -1,6 +1,7 @@
 from address_book import AddressBook
 from sort_folder import FileSorter
 from pathlib import Path
+from notess import Note, Notebook
 
 ab = AddressBook()
 while True:
@@ -30,7 +31,52 @@ while True:
         
                 
     if ff == '2':
-        pass
+        notebook = Notebook()
+        if Path(ab.filename).exists():
+            notebook.load_notes("notes.json")
+        while True:
+            commandd = input('введіть команду: ')
+            
+            if commandd == 'exit':
+                notebook.save_notes("notes.json")
+                break
+            
+            if commandd == 'add':
+                title = input()
+                text = input()
+                tags = input()
+                new_note = Note(title, text, tags)
+                notebook.add_note(new_note)
+                
+            if commandd == 'search':
+                sh_words = input('введіть нотатку яку потрібно знайти: ')
+                results = notebook.search_notes(sh_words)
+                for note in results:
+                    print(note.title)
+                    
+            if commandd == 'edit':
+                note_ed = input('введіть нотатку яку потрібно змінити: ')
+                new_title = input('')
+                new_text = input('')
+                founded_note = notebook.search_notes(note_ed)
+                if notebook.edit_note(founded_note, new_title, new_text):
+                    print("Нотатку було успішно відредаговано")
+                else:
+                    print("Нотатку з такою назвою не знайдено")
+                    
+            if command == 'show all':
+                for note in notebook.notes:
+                    print(note.title, "-", note.text)
+                    
+            if commandd == 'search by tags':
+                results = notebook.search_notes_by_tags(["план"])
+                for note in results:
+                    print(note.title)
+            
+            if commandd == 'sort':
+                notebook.sort_notes_by_tags()
+            
+            
     
     if ff == '3':
         while True:
@@ -45,7 +91,7 @@ while True:
                 file_sorter.sort_files()
             
             else:
-                print('Введіть шлях допапки: ')
+                print('Введіть шлях до папки: ')
             
 
     if ff == '4':
